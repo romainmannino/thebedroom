c14d83da000dd7a8cafda7e70993234fdc048b13
romainmannino <romainmannino@gmail.com>
2026-07-22 18:01:09 +0200

    Affiche la météo locale sur le livret public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ContactLinkEnhancer } from "@/components/contact-link-enhancer";
+import { LocalWeatherPill } from "@/components/local-weather-pill";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <ContactLinkEnhancer />
+        <LocalWeatherPill />
         {children}
       </body>
     </html>
