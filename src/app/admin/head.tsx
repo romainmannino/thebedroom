579fbdc76461f79c037427384e4076ae4190a5fa
romainmannino <romainmannino@gmail.com>
2026-08-09 22:59:01 +0200

    Use dedicated admin manifest in document head

---
+++ src/app/admin/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>The Bedroom Admin</title>
+      <meta name="application-name" content="The Bedroom Admin" />
+      <meta name="apple-mobile-web-app-title" content="Bedroom Admin" />
+      <link rel="manifest" href="/api/admin-manifest" />
+    </>
+  );
+}
